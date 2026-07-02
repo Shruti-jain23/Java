b4ab0c91da5a1afd45c457d7abb4751cf2bd8cec
Shruti-jain23 <shrutijain23705@gmail.com>
2026-07-02 14:20:24 +0530

    Added Reverse

---
+++ src/LinkedList/Reverse.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public Node rev(Node head){
+        if(head==null ||head.next==null) return head;
+        Node prev=null;
+        Node pres=head;
+        Node next=head.next;
+        while(pres!=null){
+            pres.next=prev;
+            prev=pres;
+            pres=next;
+            if(next!=null) next=next.next;
+        }
+        return prev;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Node head=Util.create(sc);
+        System.out.println("Original LinkedList : ");
+        Util.printList(head);
+        Reverse obj=new Reverse();
+        head= obj.rev(head);
+        System.out.println("Reversed LinkedList : ");
+        Util.printList(head);
+
+
+
+    }
+}
