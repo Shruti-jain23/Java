9ff0f070768c12a8c5e9abeef9bdbafd98574d4d
Shruti-jain23 <shrutijain23705@gmail.com>
2026-07-08 11:11:01 +0530

    added

---
+++ src/Tree/MaxDepth.java
@@ -0,0 +1,11 @@
+package Tree;
+
+public class MaxDepth {
+    public static int depth(TreeNode root){
+        if(root==null) return 0;
+        int left=depth(root.left);
+        int right=depth(root.right);
+        int max=Math.max(left,right)+1;
+        return max
+    }
+}
