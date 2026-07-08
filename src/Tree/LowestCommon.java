5148a4ba63568a5038e3ed8219d7f10dda187b1b
Shruti-jain23 <shrutijain23705@gmail.com>
2026-07-08 11:21:50 +0530

    added

---
+++ src/Tree/LowestCommon.java
@@ -0,0 +1,12 @@
+package Tree;
+
+public class LowestCommon {
+    public static TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
+        if(root==null) return null;
+        if(root==p || root==q) return root;
+        TreeNode left=lca(root.left,p,q);
+        TreeNode right=lca(root.right,p,q);
+        if(left!=null && right!=null) return root;
+        return (left==null)? right:left;
+    }
+}
