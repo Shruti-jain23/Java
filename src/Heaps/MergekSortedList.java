1f3059d1a4eb503c5b89dd05102e9c02b0943726
Shruti-jain23 <shrutijain23705@gmail.com>
2026-07-10 23:51:32 +0530

    added

---
+++ src/Heaps/MergekSortedList.java
@@ -0,0 +1,21 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class MergekSortedList {
+    public static void merge(ListNode[] lists){
+        PriorityQueue<ListNode> pq=new PriorityQueue<>();
+        for(ListNode temp:lists){
+            if(temp!=null)pq.offer(temp);
+        }
+        ListNode dummy=new ListNode(-1);
+        ListNode tail=dummy;
+        while(!pq.isEmpty()){
+            ListNode curr=pq.poll();
+            tail.next=curr;
+            tail=tail.next;
+            if(curr.next!=null) pq.offer(curr.next);
+        }
+        return dummy.next;
+    }
+}
